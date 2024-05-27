1cab407776fd09014f9d84135d679c70506b1ee4
jony.cunha <azevedo1safe2@gmail.com>
2024-05-27 17:44:03 -0300

    adionando caracteres acentuados

--- cifra de cesar/descriptografar.py
+++ cifra de cesar/descriptografar.py
@@ -1,7 +1,9 @@
 # Código criptográfico usando a cifra de césar[3]
 # idioma Português
 alfabeto = {'A': 'D', 'B': 'E', 'C': 'F', 'D': 'G', 'E': 'H', 'F': 'I', 'G': 'J', 'H': 'K', 'I': 'L', 'J': 'M', 'K': 'N', 'L': 'O', 'M': 'P',
-            'N': 'Q', 'O': 'R', 'P': 'S', 'Q': 'T', 'R': 'U', 'S': 'V', 'T': 'W', 'U': 'X', 'V': 'Y', 'W': 'Z', 'X': 'A', 'Y': 'B', 'Z': 'C'}
+            'N': 'Q', 'O': 'R', 'P': 'S', 'Q': 'T', 'R': 'U', 'S': 'V', 'T': 'W', 'U': 'X', 'V': 'Y', 'W': 'Z', 'X': 'A', 'Y': 'B', 'Z': 'C',
+            'É':'H','È':'H','Ê':'H','Í':'L','Ì':'L','Î':'L','Á':'D','À':'D','Â':'D','Ã':'D','Ó':'R','Ò':'R','Ô':'R','Õ':'R',
+            'Ú':'X','Ù':'X','Ç':'@'}
 
 def descriptografar(mensagem, alfabeto):
     # Guarda valores das chaves
